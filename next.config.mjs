0e33785d5e2acab2f2ceb0884a754ba590945b56
Freddy Zhang <freddyzhang@192.168.1.4>
2025-01-31 04:38:41 +0800

    Fix: Update Next.js config for Netlify deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    metadataBase: new URL("for-sherry.netlify.app"), // Replace with your actual domain
+    trailingSlash: true,
+    images: {
+      unoptimized: true, // Required if you don't use a dedicated image optimizer (for static hosting)
+    },
+  };
+  
+  export default nextConfig;
